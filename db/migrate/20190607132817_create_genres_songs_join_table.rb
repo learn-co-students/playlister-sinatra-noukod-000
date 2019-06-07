7f41a064bca0fa1b11e6b25a3be0ce7f1ed68ff2
Poliard Djimmy <poliarddjimmy@gmail.com>
2019-06-07 15:47:10 +0000

    Done.

---
+++ db/migrate/20190607132817_create_genres_songs_join_table.rb
@@ -0,0 +1,8 @@
+class CreateGenresSongsJoinTable < ActiveRecord::Migration[5.1]
+  def up 
+    create_table :song_genres do |t|
+      t.integer :song_id
+      t.integer :genre_id
+    end
+  end
+end
